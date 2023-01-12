47fa70a800dcf3c04237624778899878a9e35098
Elad Machluf <eladmahluf@gmail.com>
2023-01-12 12:59:31 +0200

    Porject Files

--- Bowling-Modul1-Final/Assets/Scripts/Pin.cs
+++ Bowling-Modul1-Final/Assets/Scripts/Pin.cs
@@ -7,20 +7,18 @@ public class Pin : MonoBehaviour
     private Rigidbody rb;
     private Ball ball;
     private GameManager gameManager;
-
+   
     
-    // Start is called before the first frame update
+    
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody>();
-        
-
+        rb = gameObject.GetComponent<Rigidbody>();     
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        ball = FindObjectOfType<Ball>();
-  
+        ball = FindObjectOfType<Ball>(); 
+
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         FreezePinPosition();
